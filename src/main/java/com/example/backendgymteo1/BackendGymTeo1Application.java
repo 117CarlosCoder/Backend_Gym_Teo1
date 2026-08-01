357e74d301c10b103f7994c0013632fc0b01eb7d
EDK367 <denilmonterroso22@gmail.com>
2026-07-31 20:08:29 -0600

    chore: set up initial application structure

---
+++ src/main/java/com/example/backendgymteo1/BackendGymTeo1Application.java
@@ -0,0 +1,13 @@
+package com.example.backendgymteo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGymTeo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGymTeo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendgymteo1/BackendGymTeo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendgymteo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGymTeo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
